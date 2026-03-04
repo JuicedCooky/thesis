6ca2781e6ab4ae42c85ed9702f41204f687b266b
JuicedCooky <alanzheng2004@outlook.com>
2026-03-04 01:43:08 -0500

    scripts

---
+++ mtil/scripts/alan_zscl_script/clean_training/2000_iter/initialize/init_trained.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#SBATCH --job-name=initialize_trained
+#SBATCH --time=00:15:00            # max time
+#SBATCH --mem=32GB                # memory
+#SBATCH --cpus-per-task=4        # number of CPU cores
+#SBATCH --gres=gpu:1
+#SBATCH --output=/scratch/alanz21/thesis/mtil/logs/%x-%j.out  # output log file
+#SBATCH --signal=USR1@60
+
+nvidia-smi
+
+module load python/3.11.5
+module load cuda/12.6
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+
+pip install --no-index --upgrade pip
+
+pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu126
+pip install tqdm ftfy regex wilds pandas
+pip install git+https://github.com/modestyachts/ImageNetV2_pytorch
+
+cd /scratch/alanz21/thesis/mtil
+
+mkdir -p logs
+
+TARGET_DATASET="DTD"
+
+SAVE_PATH="ckpt/clean/notrain/trained_2"
+
+PREV_LOAD_PATH=""
+
+
+mkdir -p ${SAVE_PATH}
+
+MODEL_NAME="${TARGET_DATASET}.pth"
+
+DATASETS="DTD"
+
+#check if model exists
+LOAD=""
+START_ITERATION=""
+if [ -f "${SAVE_PATH}/${MODEL_NAME}" ]; then
+    LOAD="--load ${SAVE_PATH}/${MODEL_NAME}"
+else
+    LOAD="--load ${PREV_LOAD_PATH}"
+    START_ITERATION="--start-iteration 0"
+fi
+
+srun python -m src.main \
+    --train-mode=whole \
+    --train-dataset=${TARGET_DATASET} \
+    --lr=1e-5\
+    --ls 0.2 \
+    --iterations 0 \
+    --method ZSCL \
+    --image_loss \
+    --text_loss \
+    --we \
+    --avg_freq 100 \
+    --l2 1 \
+    --ref-dataset ImageNet \
+    --ref-sentences conceptual_captions \
+    --save ${SAVE_PATH} \
+    --eval-datasets ${DATASETS} \
+    --eval-interval 250 \
+    --max-evaluation-size 500 
---
+++ mtil/scripts/alan_zscl_script/clean_training/2000_iter/pure_finetune/untrained/trained_finetune_no_we.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#SBATCH --job-name=un_pure_finetune_flowers
+#SBATCH --time=01:40:00            # max time
+#SBATCH --mem=32GB                # memory
+#SBATCH --cpus-per-task=4        # number of CPU cores
+#SBATCH --gres=gpu:h100:1
+#SBATCH --output=/scratch/alanz21/thesis/mtil/logs/%x-%j.out  # output log file
+#SBATCH --signal=USR1@60
+
+nvidia-smi
+
+module load python/3.11.5
+module load cuda/12.6
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+
+pip install --no-index --upgrade pip
+
+pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu126
+pip install tqdm ftfy regex wilds pandas
+pip install git+https://github.com/modestyachts/ImageNetV2_pytorch
+
+cd /scratch/alanz21/thesis/mtil
+
+mkdir -p logs
+
+
+TARGET_DATASET="Flowers"
+
+SAVE_PATH="ckpt/clean/pure_finetune/untrained/DTD_untrained/MNIST_untrained/EuroSAT_untrained/Flowers_untrained"
+
+PREV_LOAD_PATH="ckpt/clean/pure_finetune/untrained/DTD_untrained/MNIST_untrained/EuroSAT_untrained/EuroSAT.pth"
+
+GRADIENT_PATH=""
+
+mkdir -p ${SAVE_PATH}
+
+MODEL_NAME="${TARGET_DATASET}.pth"
+
+DATASETS="DTD,MNIST,EuroSAT,Flowers,ObjectNet"
+
+#check if model exists
+LOAD=""
+START_ITERATION=""
+if [ -f "${SAVE_PATH}/${MODEL_NAME}" ]; then
+    LOAD="--load ${SAVE_PATH}/${MODEL_NAME}"
+else
+    LOAD="--load ${PREV_LOAD_PATH}"
+    START_ITERATION="--start-iteration 0"
+fi
+
+srun python -m src.main \
+    --train-mode=whole \
+    --train-dataset=${TARGET_DATASET} \
+    --lr=1e-5\
+    --ls 0.2 \
+    --iterations 2000 \
+    --method finetune \
+    --image_loss \
+    --text_loss \
+    --avg_freq 100 \
+    --l2 1 \
+    --save ${SAVE_PATH} \
+    --eval-datasets ${DATASETS} \
+    --eval-interval 250 \
+    --custom-finetune \
+    --untrained \
+    --max-evaluation-size 500 \
+    ${LOAD} \
+    ${START_ITERATION}
